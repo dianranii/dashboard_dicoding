f7aaf0c04388fc2aeab9e6530f6ec7c6793f59ad
dianranii <dianmaharani1202@gmail.com>
2024-10-03 09:27:45 +0700

    Update dashboard_app.py

--- dashboard_app.py
+++ dashboard_app.py
@@ -45,7 +45,7 @@ def create_category_rating_df(df):
     
     return category_rating_df
 
-all_df = pd.read_csv("all_data.csv")
+all_df = pd.df = pd.read_excel("all_data.xlsx")
 
 all_df['order_purchase_timestamp'] = pd.to_datetime(all_df['order_purchase_timestamp'])
 
